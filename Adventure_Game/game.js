2b2dc2b7a2faa968f66083403cb7bb0e1240cc83
Unknown <preston.powell@uvu.edu>
2019-01-23 10:27:50 -0700

    Added index.html and game.js files

---
+++ Adventure_Game/game.js
@@ -0,0 +1,20 @@
+// Single line comment
+/* 
+
+Multiline comment
+
+*/
+
+// document.write("<h1>I can't wait for <em>spring!</em></h1>");
+
+
+//alert("Warning! Will Robinson...Warning!");
+
+//confirm("Do you like pokemon?");
+
+//prompt("What type of pokemon do you like?");
+
+var pc = prompt("What is your name?");
+
+confirm("So your name is "+pc);
+
